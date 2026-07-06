0b6963da5e55376b24ab442ed8a23bed18470910
MRSwaga <kopylovk@proton.me>
2026-07-06 16:25:12 +0400

    Initialize Spring Boot

---
+++ src/main/java/dev/mrswaga/ledger_core/LedgerCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.mrswaga.ledger_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerCoreApplication.class, args);
+	}
+
+}
